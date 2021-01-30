aa31f385f6c06135413bf9fdb044d8569e0c7860
Jonathan Cervas <jcervas@uci.edu>
2021-01-30 17:33:43 -0500

    Update assignBlocks.R

--- Redistricting/assignBlocks.R
+++ Redistricting/assignBlocks.R
@@ -1,14 +1,9 @@
-library(rgdal)
+# Takes a district plan file and assigns lower level geography to create a relationship file. 
 
-	
 
-us_cd <- rgdal::readOGR("/Users/user/Google Drive/GitHub/Data Files/GIS/Tigerline/2019/cd/tl_2019_us_cd116.shp")
-block_shp <- "/Users/user/Library/Mobile Documents/com~apple~CloudDocs/Downloads/tl_2019_27_tabblock10/tl_2019_27_tabblock10.shp"
-
-district_shp <- us_cd[us_cd@data$STATEFP %in% 27,]
 
 assignBlock <- function(block_shp=NA, district_shp=NA, districtID=NA, blockID=NA, district_label=NA) {
-	
+	library(rgdal)
 	if (class(district_shp) == "sp") {
 			district.shp <- district_shp
 		}  else if (class(district_shp) == "character") {
@@ -32,6 +27,12 @@ assignBlock <- function(block_shp=NA, district_shp=NA, districtID=NA, blockID=NA
 
 		return(do.call(rbind, master))
 	}
+
+# Example - Minnesota 116th Congress, Census Blocks
+	us_cd <- rgdal::readOGR("/Users/user/Google Drive/GitHub/Data Files/GIS/Tigerline/2019/cd/tl_2019_us_cd116.shp") 
+	district_shp <- us_cd[us_cd@data$STATEFP %in% 27,]
+block_shp <- "/Users/user/Library/Mobile Documents/com~apple~CloudDocs/Downloads/tl_2019_27_tabblock10/tl_2019_27_tabblock10.shp"
+
 
 a <- assignBlock(block_shp=block_shp, district_shp=district_shp, districtID="GEOID", blockID="GEOID10")
 head(a)
