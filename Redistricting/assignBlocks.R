e4fb6ecdff374d826b64560bc70f0760d10af9ad
Jonathan Cervas <jcervas@uci.edu>
2021-01-31 13:31:01 -0500

    Update assignBlocks.R

--- Redistricting/assignBlocks.R
+++ Redistricting/assignBlocks.R
@@ -18,7 +18,10 @@ assignPolys <- function(block_shp=NA, district_shp=NA, districtID=NA, blockID=NA
 		} else {
 			stop("Error: Block file not acceptable")
 		}
-		# block.shp.buffer <- raster::buffer(block.shp)
+
+		block.shp <- sp::spTransform(block.shp, raster::crs(district.shp))
+
+				# block.shp.buffer <- raster::buffer(block.shp)
 		master <- list()
 		ID <- unique(district.shp@data[,districtID])
 		for (j in 1:length(ID)) {
@@ -38,8 +41,8 @@ assignPolys <- function(block_shp=NA, district_shp=NA, districtID=NA, blockID=NA
 
 # To set up, go to mapshaper.org and put in the Census block polygon. In the Console, enter the command "-points inner". Export to shapefile, and use this new point-based file for "block_shp"
 # Example - Minnesota 116th Congress, Census Blocks
-	district_shp <- rgdal::readOGR("/Users/user/Library/Mobile Documents/com~apple~CloudDocs/Downloads/tl_2019/tl_2019_us_cd116.shp") 
-	block_shp <- rgdal::readOGR("/Users/user/Library/Mobile Documents/com~apple~CloudDocs/Downloads/mn_points_inner/mn_points_inner.shp")
+	district_shp <- rgdal::readOGR("/Users/user/Library/Mobile Documents/com~apple~CloudDocs/Downloads/tl_2019_us_cd116/tl_2019_us_cd116.shp")
+	block_shp <- rgdal::readOGR("/Users/user/Library/Mobile Documents/com~apple~CloudDocs/Downloads/tl_2019_27_tabblock10 2/tl_2019_27_tabblock10.shp")
 block_poly <- rgdal::readOGR("/Users/user/Google Drive/GitHub/Data Files/GIS/Tigerline/2019/blocks/27/tl_2019_27_tabblock10.shp")
 
 a <- assignPolys(block_shp=block_shp, district_shp=district_shp, districtID="GEOID", blockID="GEOID10")
@@ -60,8 +63,8 @@ mn_blocks@data$cols[is.na(mn_blocks@data$cols)] <- "#000000"
 
 
 png("/Users/user/Library/Mobile Documents/com~apple~CloudDocs/Downloads/map.png", height = 1600*10, width = 900*10, units = "px", pointsize = 12)
-plot(district_shp, border="gray50", lwd=3)
-	plot(mn_blocks, border = "#FFFFFF", col = mn_blocks@data$cols, lty=1, lwd = 1.5, add=T)
+# plot(district_shp, border="gray50", lwd=3)
+	plot(mn_blocks, border = "#FFFFFF", col = mn_blocks@data$cols, lty=1, lwd = 1.5)
 dev.off()
 
 # x <- block_poly
