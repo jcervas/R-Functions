4f2c1b381e9f4b6d5f12817060e635d0a520f9c2
Jonathan Cervas <jcervas@uci.edu>
2021-01-30 23:37:34 -0500

    Update assignBlocks.R

--- Redistricting/assignBlocks.R
+++ Redistricting/assignBlocks.R
@@ -18,13 +18,13 @@ assignPolys <- function(block_shp=NA, district_shp=NA, districtID=NA, blockID=NA
 		} else {
 			stop("Error: Block file not acceptable")
 		}
-
+		block.shp.buffer <- raster::buffer(block.shp)
 		master <- list()
 		ID <- unique(district.shp@data[,districtID])
 		for (j in 1:length(ID)) {
 			poly.tmp <- district.shp[district.shp@data[,districtID] %in% ID[j],]
 			# blocks.subset <- block.shp[poly.tmp,] #subset in base R
-			blocks.subset <- raster(block.shp)[poly.tmp,] #subset in base R, add buffer from raster
+			blocks.subset <- block.shp.buffer[poly.tmp,] #subset in base R, add buffer from raster
 			# blocks.subset <- rmapshaper::ms_clip(block.shp, poly.tmp) #subset using rmapshaper
 			# x <- raster::intersect(block.shp, poly.tmp) # subset using raster
 			# blocks.subset <- rgeos::gIntersection(block.shp, poly.tmp) # subset using rgeos
@@ -40,7 +40,7 @@ assignPolys <- function(block_shp=NA, district_shp=NA, districtID=NA, blockID=NA
 	us_cd <- rgdal::readOGR("/Users/user/Google Drive/GitHub/Data Files/GIS/Tigerline/2019/cd/tl_2019_us_cd116.shp") 
 	district_shp <- us_cd[us_cd@data$STATEFP %in% 27,]
 block_shp <- "/Users/user/Library/Mobile Documents/com~apple~CloudDocs/Downloads/tl_2019_27_tabblock10/tl_2019_27_tabblock10.shp"
-block_shp <- "/Users/user/Google Drive/GitHub/Data Files/GIS/Tigerline/2019/blocks/27/tl_2019_27_tabblock10.shp"
+# block_shp <- "/Users/user/Google Drive/GitHub/Data Files/GIS/Tigerline/2019/blocks/27/tl_2019_27_tabblock10.shp"
 
 a <- assignPolys(block_shp=block_shp, district_shp=district_shp, districtID="GEOID", blockID="GEOID10")
 head(a)
