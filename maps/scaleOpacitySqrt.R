7d16c073ab78a6bd4fa908ed0502a3796268100a
Jonathan Cervas <jcervas@andrew.cmu.edu>
2024-06-28 11:43:50 -0400

    Update scaleOpacitySqrt.R

--- maps/scaleOpacitySqrt.R
+++ maps/scaleOpacitySqrt.R
@@ -28,16 +28,16 @@ scaleOpacitySqrt <- function(value, minOpacity = 0.25, maxOpacity = 0.75, minDom
   }
 }
 
-# Example usage with the provided data frame
-data <- data.frame(per_point_diff = c(-0.1, -0.05, -0.01, 0, 0.1, 0.1))
+# # Example usage with the provided data frame
+# data <- data.frame(per_point_diff = c(-0.1, -0.05, -0.01, 0, 0.1, 0.1))
 
-# Find the min and max of the data to set the domain
-minDomain <- min(data$per_point_diff)
-maxDomain <- max(data$per_point_diff)
+# # Find the min and max of the data to set the domain
+# minDomain <- min(data$per_point_diff)
+# maxDomain <- max(data$per_point_diff)
 
-# Apply the function to the data frame
-data$opacity_value <- sapply(data$per_point_diff, scaleOpacitySqrt, minOpacity = 0.25, maxOpacity = 0.75, minDomain = minDomain, maxDomain = maxDomain)
-data$opacity_hex <- sapply(data$per_point_diff, scaleOpacitySqrt, minOpacity = 0.25, maxOpacity = 0.75, minDomain = minDomain, maxDomain = maxDomain, return = "hex")
+# # Apply the function to the data frame
+# data$opacity_value <- sapply(data$per_point_diff, scaleOpacitySqrt, minOpacity = 0.25, maxOpacity = 0.75, minDomain = minDomain, maxDomain = maxDomain)
+# data$opacity_hex <- sapply(data$per_point_diff, scaleOpacitySqrt, minOpacity = 0.25, maxOpacity = 0.75, minDomain = minDomain, maxDomain = maxDomain, return = "hex")
 
-# Print the results
-print(data)
+# # Print the results
+# print(data)
