d04e1159117cda03c718ecf349c16b4569301a03
Jonathan Cervas <jcervas@andrew.cmu.edu>
2024-07-08 16:29:00 -0400

    Update getPresData.R

--- getPresData.R
+++ getPresData.R
@@ -26,11 +26,11 @@ pres.data <- pres.data.lag <- pres.data.lag2 <- cbind.data.frame(
 	ecvotes=as.numeric(election.dta$ecvotes))
 
 # Sort the data by State and Year
-data <- pres.data[order(data$state, pres.data$year), ]
+pres.data <- pres.data[order(data$state, pres.data$year), ]
 
 # Create the lagged variable within each state
-data$dlag <- ave(data$dem, data$state, FUN = function(x) c(NA, x[-length(x)]))
-data$dlag2 <- ave(data$dlag, data$state, FUN = function(x) c(NA, x[-length(x)]))
+pres.data$dlag <- ave(pres.data$dem, pres.data$state, FUN = function(x) c(NA, x[-length(x)]))
+pres.data$dlag2 <- ave(pres.data$dlag, pres.data$state, FUN = function(x) c(NA, x[-length(x)]))
 
 
 pop <- jsonlite::fromJSON("https://raw.githubusercontent.com/jcervas/Data/master/Elections/hist_pop.json")
