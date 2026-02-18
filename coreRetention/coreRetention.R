c99ed701fca7d9368392445a7ebeebd80bde1582
Jonathan Cervas <jcervas@andrew.cmu.edu>
2026-02-18 10:32:10 -0600

    Refactor district_core function by removing old code
    Removed the original implementation of the district_core function, which calculated the overlap of populations between districts.

--- coreRetention/coreRetention.R
+++ coreRetention/coreRetention.R
@@ -1,30 +1,4 @@
 
-district_core <- function(df, base_plan, plan, pop_col = NULL) {
-
-  base  <- df[[base_plan]]
-  other <- df[[plan]]
-
-  if (is.null(pop_col)) {
-    w <- rep(1, nrow(df))
-  } else {
-    w <- df[[pop_col]]
-  }
-
-  base_districts  <- sort(unique(base))
-  other_districts <- sort(unique(other))
-
-  sapply(base_districts, function(d) {
-    idx_base <- base == d
-    base_pop <- sum(w[idx_base], na.rm = TRUE)
-
-    overlaps <- sapply(other_districts, function(o) {
-      sum(w[idx_base & other == o], na.rm = TRUE)
-    })
-
-    max(overlaps, na.rm = TRUE) / base_pop
-  })
-}
-
 
 district_core <- function(df, base_plan, plan, pop_col = NULL) {
 
